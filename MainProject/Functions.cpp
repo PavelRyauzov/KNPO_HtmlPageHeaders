e6201386bfd85a6f92edfa0c7328a2289064c2d9
PavelRyauzov <pavel.ryauzov@gmail.com>
2021-05-19 21:27:50 +0300

    Реализована функция разбора тега заголовка

--- MainProject/Functions.cpp
+++ MainProject/Functions.cpp
@@ -39,6 +39,128 @@ QStringList getHeaderTags(QStringList htmlCode)
     return headerTags;
 }
 
+Header parseHeaderTag(QString headerTag)
+{
+    // Регулярные выражения для тегов заголовков с атрибутами и без атрибутов
+    QRegExp rxHeaderTagWithoutAttribute("<h1>([^<]*)</h1>|<h2>([^<]*)</h2>|<h3>([^<]*)</h3>|<h4>([^<]*)</h4>|<h5>([^<]*)</h5>|<h6>([^<]*)</h6>|<H1>([^<]*)</H1>|<H2>([^<]*)</H2>|<H3>([^<]*)</H3>|<H4>([^<]*)</H4>|<H5>([^<]*)</H5>|<H6>([^<]*)</H6>");
+    QRegExp rxHeaderTagWithAttribute("<h1( *)([^<]*)>([^<]*)</h1>|<h2( *)([^<]*)>([^<]*)</h2>|<h3( *)([^<]*)>([^<]*)</h3>|<h4( *)([^<]*)>([^<]*)</h4>|<h5( *)([^<]*)>([^<]*)</h5>|<h6( *)([^<]*)>([^<]*)</h6>|<H1( *)([^<]*)>([^<]*)</H1>|<H2( *)([^<]*)>([^<]*)</H2>|<H3( *)([^<]*)>([^<]*)</H3>|<H4( *)([^<]*)>([^<]*)</H4>|<H5( *)([^<]*)>([^<]*)</H5>|<H6( *)([^<]*)>([^<]*)</H6>");
+
+    //Регулярные выражения для открывающихся тегов заголовка и закрывающихся
+    QRegExp rxOpenTag("h[1-6]|H[1-6]");
+    QRegExp rxCloseTag("/h[1-6]|/H[1-6]");
+
+    // Создать переменную структуры Header, сюда будем помещать результат - разобранный заголовок
+    Header parsedHeader;
+
+    // Если в строке с тегом содержится тег "H" без аттрибута
+    if (rxHeaderTagWithoutAttribute.exactMatch(headerTag))
+    {
+        // Найти и записать в качестве результата уровень вложенности и текст тега заголовка...
+
+
+        int lastPos = 0;
+
+        // Если в строке с тегом  найден тег заголовка без аттрибута
+        if ((lastPos = rxHeaderTagWithoutAttribute.indexIn(headerTag, lastPos)) != -1)
+        {
+            // Определить текст внутри тега
+            int pos = 0;
+
+            // Создать строку буффер
+            QString temp;
+
+            // Если в строке с тегом  найден открывающийся тег заголовка
+            if ((pos = rxOpenTag.indexIn(headerTag, pos)) != -1)
+            {
+                // Записать строку с тегом в строку буффер
+                temp = headerTag;
+
+                // Удалить открывающийся тег из строки буффера
+                temp.remove(pos - 1, pos + 3);
+            }
+
+            pos = 0;
+
+            // Если в строке буффер найден закрывающийся тег заголовка
+            if ((pos = rxCloseTag.indexIn(temp, pos)) != -1)
+            {
+                // Удалить закрывающйися тег из строки буффера
+                temp.remove(pos - 1, pos + 4);
+            }
+
+            // Удалить пробелы в начале и в конце строки буффера
+            temp = temp.trimmed();
+
+            // Записать текст, что остался в строке буффера в качестве результата - текст внутри тега "H"
+            parsedHeader.content = temp;
+
+            // Определить уровень вложенности тега
+            pos = 0;
+
+            // Если в строке с тегом  найден открывающийся тег заголовка
+            if ((pos = rxOpenTag.indexIn(headerTag, pos)) != -1)
+            {
+                // Записать 2 символ строки с тегом в качестве результата - уровень вложенности тега "H"
+                parsedHeader.nestingLevel = QString(rxOpenTag.cap(0)[1]).toInt();
+            }
+        }
+    }
+    // Иначе если в строке с тегом содержится тег "H" с аттрибутом
+    else if (rxHeaderTagWithAttribute.exactMatch(headerTag))
+    {
+        int lastPos = 0;
+
+        // Если в строке найден тег заголовка с аттрибутом
+        if ((lastPos = rxHeaderTagWithAttribute.indexIn(headerTag, lastPos)) != -1)
+        {
+            // Определить текст внутри тега
+            int pos = 0;
+
+            // Создать строку буффер
+            QString temp;
+
+            QRegExp closeBracket(">");
+
+            // Если в строке с тегом  найден конец закрывающегося тега заголовка
+            if ((pos = closeBracket.indexIn(headerTag, pos)) != -1)
+            {
+                // Записать строку с тегом в строку буффер
+                temp = headerTag;
+
+                 // Удалить открывающийся тег из строки буффера
+                temp.remove(0, pos + 1);
+            }
+
+            pos = 0;
+
+            // Если в строке буффер найден закрывающийся тег заголовка
+            if ((pos = rxCloseTag.indexIn(temp, pos)) != -1)
+            {
+                 // Удалить закрывающйися тег из строки буффера
+                temp.remove(pos - 1, pos + 4);
+            }
+
+            // Удалить пробелы в начале и в конце строки буффера
+            temp = temp.trimmed();
+
+            // Записать текст, что остался в строке буффера в качестве результата - текст внутри тега "H"
+            parsedHeader.content = temp;
+
+            // Определить уровень вложенности тега
+            pos = 0;
+
+             // Если в строке с тегом  найден открывающийся тег заголовка
+            if ((pos = rxOpenTag.indexIn(headerTag, pos)) != -1)
+            {
+                // Записать 2 символ строки с тегом в качестве результата - уровень вложенности тега "H"
+                parsedHeader.nestingLevel = QString(rxOpenTag.cap(0)[1]).toInt();
+            }
+        }
+    }
+
+    return parsedHeader;
+}
+
 void getFileContent(QString path, QStringList &fileContent)
 {
     QFile mFile(path);
